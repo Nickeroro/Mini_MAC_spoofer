f809cc65ed322cb09cada94d8dbbbe6c5deeee0d
Nicolas <37118153+Nickeroro@users.noreply.github.com>
2020-01-29 21:22:52 +0100

    01292020-1025

--- MAC_changer.sh
+++ MAC_changer.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# This is a comment!
-sudo ip link set dev enp4s0 down
-sudo ip link set dev enp4s0 address 3c:d9:2b:68:88:e8
-sudo ip link set dev enp4s0 up
-#sudo ip address add 172.20.12.220/24 dev enp4s0
-export http_proxy=http://192.168.1.16:8080/
-export HTTP_PROXY=http://192.168.1.16:8080/
-export ftp_proxy=http://192.168.1.16:8080/
-export FTP_PROXY=http://192.168.1.16:8080/
-export all_proxy=http://192.168.1.16:8080/
-export ALL_PROXY=http://192.168.1.16:8080/
-export https_proxy=http://192.168.1.16:8080/
-export HTTPS_PROXY=http://192.168.1.16:8080/
+INTERFACE=$1
 
+sudo ip link set dev $INTERFACE down
+sudo ip link set dev $INTERFACE address $(openssl rand -hex 1):$(openssl rand -hex 1):$(openssl rand -hex 1):$(openssl rand -hex 1):$(openssl rand -hex 1):$(openssl rand -hex 1)
+sudo ip link set dev $INTERFACE up
+
+
+# export http_proxy=http://192.168.1.16:8080/
+# export HTTP_PROXY=http://192.168.1.16:8080/
+# export ftp_proxy=http://192.168.1.16:8080/
+# export FTP_PROXY=http://192.168.1.16:8080/
+# export all_proxy=http://192.168.1.16:8080/
+# export ALL_PROXY=http://192.168.1.16:8080/
+# export https_proxy=http://192.168.1.16:8080/
+# export HTTPS_PROXY=http://192.168.1.16:8080/
 
 echo " _____________________________________"
 echo "< Your MAC Adress has been spoofed <3 >"
